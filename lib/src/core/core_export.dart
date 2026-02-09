bcbab7ba8b02cf294f5d22ee610cfbf8e50cf184
moury12 <tanzibamouri00@gmail.com>
2026-02-09 13:03:32 +0600

    style: app colors added

---
+++ lib/src/core/core_export.dart
@@ -0,0 +1,1 @@
+export 'theme/theme_export.dart';
---
+++ lib/src/core/theme/app_colors.dart
@@ -0,0 +1,11 @@
+import '../../src_export.dart';
+
+class AppColors {
+  static const Color kPrimaryColor = Color(0xFFA59BF9);
+  static const Color kSecondaryColor = Color(0xFF8FBCF4);
+  static const Color kAccentColor = Color(0xFFEEECFF);
+  static const Color kRedColor = Color(0xFFEF4444);
+  static const Color kGreenColor = Color(0xFF22C55E);
+  static const Color kBackgroundColor = Colors.white;
+  static const Color kTextColor = Color(0xFF111827);
+}
