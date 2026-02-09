8eb8ede1287aa245442fdb9fb660da5da7d31891
moury12 <tanzibamouri00@gmail.com>
2026-02-09 16:59:16 +0600

    style: feature architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grabby_app/src/featurs/onboarding-splash/presentation/pages/splash_page.dart';
 import 'src/core/theme/theme.dart';
 
 void main() {
@@ -13,50 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Grabby App',
       theme: AppTheme.getLightTheme(context),
-      home: const MyHomePage(title: 'Grabby Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(widget.title)),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      home: SplashPage(),
     );
   }
 }
